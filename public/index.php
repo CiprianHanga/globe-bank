8ec836baef615279b92d12c117c03bfc64b1e757
Ciprian Hanga <ciprianhanga@gmail.com>
2019-05-31 01:28:08 +0300

    Added basic styling.

--- public/index.php
+++ public/index.php
@@ -2,11 +2,26 @@
 
 <html lang="en">
   <head>
-    <title>Page Title</title>
+    <title>GBI</title>
     <meta charset="utf-8">
   </head>
 
   <body>
-    
+    <header>
+      <h1>Globe Bank: Coming Soon </h1>
+    </header>
+
+    <navigation>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </navigation>
+
+    <div id="content">
+    </div>
+
+    <footer>
+      &copy; <?php echo date('Y'); ?> Globe Bank
+    </footer>
   </body>
 </html>
--- public/staff/index.php
+++ public/staff/index.php
@@ -2,11 +2,27 @@
 
 <html lang="en">
   <head>
-    <title>Page Title</title>
+    <title>GBI</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
   </head>
 
   <body>
-    
+    <header>
+      <h1>GBI Staff Area</h1>
+    </header>
+
+    <navigation>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </navigation>
+
+    <div id="content">
+    </div>
+
+    <footer>
+      &copy; <?php echo date('Y'); ?> Globe Bank
+    </footer>
   </body>
 </html>
