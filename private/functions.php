4d4fee10f97c90904618c31d6d6936a6c00012de
Ciprian Hanga <ciprianhanga@gmail.com>
2019-06-10 00:47:06 +0300

    Created custom function to protect the values passed in with HTML.

--- private/functions.php
+++ private/functions.php
@@ -15,3 +15,7 @@ function u($string="") {
 function raw_u($string="") {
     return rawurlencode($string);
 }
+
+function h($string="") {
+    return htmlspecialchars($string);
+}
--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -4,7 +4,7 @@ require_once('../../../private/initialize.php');
 $id = isset($_GET['id']) ? $_GET['id'] : '1';
 // $id = $_GET['id'] ?? '1'; // PHP > 7.0
 
-//echo $id;
+echo h($id);
 
 ?>
 
