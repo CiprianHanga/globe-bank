b79496619fe9260257ddaf64024d93be04cf59b9
Ciprian Hanga <ciprianhanga@gmail.com>
2019-06-10 00:56:22 +0300

    Very basic custom HTTP headers for 404 and 500.

--- private/functions.php
+++ private/functions.php
@@ -19,3 +19,13 @@ function raw_u($string="") {
 function h($string="") {
     return htmlspecialchars($string);
 }
+
+function error_404() {
+    header($_SERVER['SERVER_PROTOCOL'] . " 404 Not Found");
+    exit();
+}
+
+function error_500() {
+    header($_SERVER['SERVER_PROTOCOL'] . " 500 Internal Server Error");
+    exit();
+}
---
+++ public/staff/subjects/new.php
@@ -0,0 +1,12 @@
+<?php
+require_once('../../../private/initialize.php');
+$test = isset($_GET['test']) ? $_GET['test'] : '';
+
+
+if($test == '404') {
+    error_404();
+} elseif($test == '500') {
+    error_500();
+} else {
+    echo 'No error';
+}
