0546be7f16400b2ca3c8048d8fbf722fae478add
Ciprian Hanga <ciprianhanga@gmail.com>
2019-06-25 19:58:17 -0400

    Added index file w/ redirect to root folder for dev on Linux (temp).

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+// echo 'hello din globe-bank';
+header('Location: '.$uri.'/globe-bank/public/');
+exit;
+
+?>
+Something went terribly wrong... :-(
